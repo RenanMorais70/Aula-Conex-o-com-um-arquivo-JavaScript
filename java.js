b0b3d25d099215be9132827e7ca7344e348af1c8
Renan <remorais70@gmail.com>
2023-11-06 16:27:15 -0300

    aula de conexão com um arquivo javaScript

---
+++ java.js
@@ -0,0 +1,9 @@
+// Define uma função que imprime a mensagem "Conexão feita com sucesso!" no terminal
+function imprimirMensagem() {
+    console.log('Conexão feita com sucesso!');
+}
+
+// Executa a função três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
